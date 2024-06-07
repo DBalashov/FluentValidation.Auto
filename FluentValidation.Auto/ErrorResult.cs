36e4a9f4e0001f7ba11b2d3b514aca885a2ab577
dbalashov <dbalashov@gmail.com>
2024-06-07 14:33:50 +0400

    +optional AddAutoValidationError

---
+++ FluentValidation.Auto/ErrorResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FluentValidation.Auto;
+
+public sealed record ErrorResult([property: JsonPropertyName("traceId")]
+                                 string TraceId,
+                                 [property: JsonPropertyName("title")]  string                        Title,
+                                 [property: JsonPropertyName("status")] int                           Status = 400,
+                                 [property: JsonPropertyName("type")]   string                        Type   = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                                 [property: JsonPropertyName("errors")] Dictionary<string, string[]>? Errors = null);
--- FluentValidation.Auto/Register.cs
+++ FluentValidation.Auto/Register.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FluentValidation.Auto;
 
@@ -8,5 +11,30 @@ public static class Register
     {
         o.ModelValidatorProviders.Add(new ModelValidatorProvider());
         return o;
+    }
+    
+    public static IMvcBuilder AddAutoValidationError(this IMvcBuilder builder, string messageTitle = "Validation error")
+    {
+        builder.ConfigureApiBehaviorOptions(o => o.InvalidModelStateResponseFactory =
+                                                     ctx =>
+                                                     {
+                                                         var logger = ctx.HttpContext.RequestServices.GetRequiredService<ILogger<IMvcBuilder>>();
+                                                         
+                                                         if (ctx.ModelState.ValidationState != ModelValidationState.Invalid)
+                                                             return new ObjectResult(new ErrorResult(ctx.HttpContext.TraceIdentifier, "Unknown error")) {StatusCode = 400};
+                                                         
+                                                         var err400 = ctx.ModelState
+                                                                         .Where(p => p.Value is {ValidationState: ModelValidationState.Invalid})
+                                                                         .GroupBy(p => p.Key)
+                                                                         .ToDictionary(p => p.Key,
+                                                                                       p => p.First().Value!.Errors.Select(c => c.ErrorMessage).ToArray());
+                                                         
+                                                         var errLog = string.Join(", ", err400.Select(p => p.Key + ": " + string.Join(",", p.Value)));
+                                                         logger.LogWarning("{0}: {1} -> {2}", ctx.HttpContext.Request.Method, ctx.HttpContext.Request.Path, errLog);
+                                                         
+                                                         return new ObjectResult(new ErrorResult(ctx.HttpContext.TraceIdentifier, messageTitle, Errors: err400)) {StatusCode = 400};
+                                                     });
+        
+        return builder;
     }
 }
